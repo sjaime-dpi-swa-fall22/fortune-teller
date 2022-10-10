d6f1711e8e2d9fd45134324615cbda7bc19935cc
sjaime-dpi <sjaime@uillinois.edu>
2022-10-10 15:28:57 +0000

    debugged first 4 "gets" in routes.rb

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({ :template => "lottery_stuff/unlucky_nums.html.erb"})
+  end
 end
